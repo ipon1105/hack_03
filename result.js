7aa20dacfbb7f8d158686393217b334eb1cc2474
Akindin <88840107+Akindin@users.noreply.github.com>
2023-05-13 13:43:34 +0300

    Created class for result

---
+++ result.js
@@ -0,0 +1,14 @@
+class Result {
+    constructor() {
+        this.titleTime = null; // Время появления заголовка в браузере
+        this.renderTime = null; // Время появления контента в браузере
+        this.interactivTime = null; // Время до интерактивности
+        this.firstByte = null; // Время первого байта
+        this.lastByte = null; // Время последнего байта
+        this.countByte = null; // Общее количество байта
+        this.DNSTime = null; // Время поиска DNS
+        this.connectionTime = null; // Время соединения
+    }
+
+    
+}
